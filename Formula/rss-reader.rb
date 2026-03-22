2c1adaa532c94267ea81add300087daaf5e44a14
moyang <moyang@moyangdeMacBook-Pro.local>
2026-03-22 15:07:12 +0800

    chore: bump rss-reader to v0.1.1

--- Formula/rss-reader.rb
+++ Formula/rss-reader.rb
@@ -1,8 +1,8 @@
 class RssReader < Formula
   desc "TUI RSS/Atom reader with multi-feed support"
   homepage "https://github.com/MOHY-Li/rss-reader"
-  url "https://api.github.com/repos/MOHY-Li/rss-reader/tarball/v0.1.0"
-  sha256 "d80d733d4b0503704418ce23f78c5acfd5f34730688e25d9df22ad4bf4d702a0"
+  url "https://api.github.com/repos/MOHY-Li/rss-reader/tarball/v0.1.1"
+  sha256 "8854defc9e7676a3ae7c86047b54d0d8ad424026512c065b35c17bdc46e51bc5"
   depends_on "rust" => :build
 
   def install
