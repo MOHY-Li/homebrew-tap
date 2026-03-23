edd66ed1fa6e83b8a7bc79d362be716476d187e8
moyang <moyang@moyangdeMacBook-Pro.local>
2026-03-23 12:00:37 +0800

    chore: fix v0.1.3 sha256

--- Formula/rss-reader.rb
+++ Formula/rss-reader.rb
@@ -2,7 +2,7 @@ class RssReader < Formula
   desc "TUI RSS/Atom reader with multi-feed support"
   homepage "https://github.com/MOHY-Li/rss-reader"
   url "https://github.com/MOHY-Li/rss-reader/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "3b34870dca38283104591826a711f05fc375dd85b728bdd5203f2e3aefd7cb60"
+  sha256 "a553d162d814986419f5727c7ff1a73ef12561d700297489e2d4274bca5525e2"
   depends_on "rust" => :build
 
   def install
